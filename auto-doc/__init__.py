b3ec66852fe5cc9d6eeb38585816f04980eab839
AdebayoBraimah <adebayo.braimah@gmail.com>
2022-04-21 10:38:40 -0600

    ENH: Added support for reading version number from file

---
+++ auto-doc/__init__.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+"""Python package that wraps ``sphinx`` documentation package for creating HTML documentation.
+"""
+import os
+from typing import List
+
+name: str = "auto-doc"
+
+_MISCDIR: str = os.path.abspath("misc")
+
+_version_file: str = os.path.abspath(os.path.join(_MISCDIR, "version.txt"))
+
+with open(_version_file, "r") as f:
+    file_contents: str = f.read()
+    _version: str = file_contents.strip("\n")
+
+__author__: str = "Adebayo Braimah"
+__maintainer__: str = __author__
+__credits__: List[str] = [
+    "Adebayo Braimah",
+    "Cincinnati Children's Hospital Medical Center",
+    "Imaging Research Center",
+    "CCHMC Dept. of Radiology",
+]
+__license__: str = "GPL"
+__version__: str = _version
+__maintainer__: str = "Adebayo Braimah"
+__email__: str = "adebayo.braimah@gmail.com"
+__status__: str = "Development"
+
+# NOTE:
+#
+# More information about organizing author information:
+#   * https://stackoverflow.com/questions/1523427/what-is-the-common-header-format-of-python-files
+#   * http://epydoc.sourceforge.net/manual-fields.html#module-metadata-variables
