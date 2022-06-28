488cc5880724b415666b3fe8a604b70aec513435
AdebayoBraimah <adebayo.braimah@gmail.com>
2022-06-28 13:41:05 -0400

    ENH: Added support for writing index file

--- autodoc/build/initdocs/index.py
+++ autodoc/build/initdocs/index.py
@@ -2,10 +2,55 @@
 """
 
 from commandio.workdir import WorkDir
+from autodoc.utils.util import write_file
 
 
-def _init_index(outdir: str):
+def write_index(outdir: str, pkg: str) -> str:
+    """Creates index.rst file for sphinx.
+
+    Args:
+        outdir: Output parent directory.
+        pkg: Package name.
+
+    Returns:
+        Index.rst absolute file path.
+    """
+    _IDX_TEXT: str = f""".. {pkg} documentation master file.
+You can adapt this file completely to your liking, but it should at least
+contain the root `toctree` directive.
+
+Welcome to {pkg}'s documentation!
+===============================
+
+.. toctree::
+   :maxdepth: 3
+
+    .. # ADD RST FILES HERE
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
+    """
+
+    idx: str = _init_index(outdir=outdir)
+    write_file(idx, text=_IDX_TEXT, num_spaces=4, mode="a")
+    return idx
+
+
+def _init_index(outdir: str) -> str:
+    """Helper function that creates index.rst file for sphinx.
+
+    Args:
+        outdir: Output parent directory.
+
+    Returns:
+        Index.rst absolute file path.
+    """
     with WorkDir(outdir) as od:
         sourcedir: str = od.join("source")
         with WorkDir(sourcedir) as sd:
             idx: str = sd.join("index.rst")
+    return idx
