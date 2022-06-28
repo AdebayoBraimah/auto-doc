5a97c84a5d675d32d1c492fe3a81a11ed6e7cb63
AdebayoBraimah <adebayo.braimah@gmail.com>
2022-06-28 13:12:50 -0400

    RF: Renamed for python package compliance

--- autodoc/build/initdocs/index.py
+++ autodoc/build/initdocs/index.py
@@ -2,11 +2,10 @@
 """
 
 from commandio.workdir import WorkDir
-from 
+
 
 def _init_index(outdir: str):
     with WorkDir(outdir) as od:
-        sourcedir: str = od.join('source')
+        sourcedir: str = od.join("source")
         with WorkDir(sourcedir) as sd:
-            idx: str = sd.join('index.rst')
-
+            idx: str = sd.join("index.rst")
