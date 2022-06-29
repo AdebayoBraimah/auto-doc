9d2c3b669c657fdcb51500c9027f01930ca573fc
AdebayoBraimah <adebayo.braimah@gmail.com>
2022-06-29 16:23:06 -0400

    RF: Added newlines to increase readability

--- autodoc/doccode/sphinxapi.py
+++ autodoc/doccode/sphinxapi.py
@@ -16,10 +16,14 @@ def sphinx_apidoc(outdir: str, pkg_path: str) -> None:
         pkg_path: Package path.
     """
     cwd: str = os.getcwd()
+
     with WorkDir(outdir) as od:
         docdir: str = od.join("doc")
+
     os.chdir(docdir)
+
     sphinx: Command = Command(f"sphinx-apidoc -o source {pkg_path}")
     sphinx.run()
+
     os.chdir(cwd)
     return None
