081f93b6716e4dba0b1fbe5885636e95e2552133
AdebayoBraimah <adebayo.braimah@gmail.com>
2022-06-29 15:24:11 -0400

    ENH: Added function combines searching for scripts and writing rst files

--- autodoc/doccode/shell.py
+++ autodoc/doccode/shell.py
@@ -3,7 +3,46 @@
 import os
 from typing import Set
 
-from autodoc.utils.docshell import _auto_detect as auto_detect
+from commandio.fileio import File
+from commandio.workdir import WorkDir
+from autodoc.utils.docshell import document_shell_script, _auto_detect as auto_detect
+
+
+def write_script_docs(pkg_dir: str, outdir: str) -> Set[str]:
+    """Writes reStructered Text files (shell) scripts.
+
+    NOTE:
+        * ``out_dir`` is assumed to be the main/parent directory of the repository.
+        * If any reStructered Text files of package scripts exist in the output source directory, then those files will be overwritten.
+
+    Args:
+        pkg_dir: Path to package/repository.
+        outdir: Path to output directory.
+
+    Returns:
+        Set of strings that corresponds to output  reStructered Text files
+    """
+    scripts: Set[str] = file_search(pkg_dir=pkg_dir)
+    rsts: Set[str] = set()
+
+    with WorkDir(outdir) as od:
+        srcdir: str = od.join("doc", "source")
+
+        with WorkDir(srcdir) as sd:
+
+            for script in scripts:
+                with File(script) as scr:
+                    _, fname, _ = scr.file_parts()
+                    outfile: str = sd.join(f"{fname}.rst")
+
+                document_shell_script(
+                    file=script,
+                    outfile=outfile,
+                    convert_tabs_to_spaces=True,
+                    num_spaces=4,
+                )
+                rsts.add(outfile)
+    return rsts
 
 
 def file_search(pkg_dir: str) -> Set[str]:
